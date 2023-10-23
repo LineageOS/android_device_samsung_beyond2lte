7d3cb2ccbe154afae21526b2c6c76a593c0052f1
Tim Zimmermann <tim@linux4.de>
2023-10-23 07:22:28 +0200

    beyond2lte: Move audio mixer configs to devicetree
    * Also move floating_feature.xml to the correct section
    
    Change-Id: Ia3188800121111e48f62025e51c23241f324e466

--- device.mk
+++ device.mk
@@ -23,5 +23,10 @@ $(call inherit-product, vendor/samsung/beyond2lte/beyond2lte-vendor.mk)
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
+# Audio
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/audio/mixer_gains.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_gains.xml \
+    $(LOCAL_PATH)/configs/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
